36e6e73c642c14df94f329df77d7334b3a6ccdac
Edison Garcia <egachi@live.com>
2019-04-10 17:59:56 -0500

    Updating to azure

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   server: {
     appId: process.env.APP_ID || 'egachi',
-    masterKey: process.env.MASTER_KEY || 'Passw0rd123456%',
+    masterKey: process.env.MASTER_KEY || 'Passw0rd1234560',
     databaseURI: process.env.DATABASE_URI || 'mongodb://edisga:toZpQmJKtCJo8Fq3e9nqRg5XVYamfGoV6ZaH01AWzWLTptlj81Rmcz7YufFhWMaJVLyOhG9JUIyORXhzVNbcvQ==@edisga.documents.azure.com:10255/dev?ssl=true&replicaSet=globaldb',
     serverURL: (process.env.SERVER_URL || 'http://edisga-parse.azurewebsites.net') + '/parse',
   },
