e3aa49829d9e48b56b460d8db34fa207178e5f75
Edison Garcia <egachi@live.com>
2019-07-17 22:39:20 -0500

    Updating to the latest versions and removing the nodejs version from package.json

--- config.js
+++ config.js
@@ -1,23 +1,23 @@
 module.exports = {
   server: {
-    appId: process.env.APP_ID || 'username',
-    masterKey: process.env.MASTER_KEY || '*******',
-    databaseURI: process.env.DATABASE_URI || 'mongodb://********************@[server].documents.azure.com:10255/[dbname]?ssl=true&replicaSet=globaldb',
-    serverURL: (process.env.SERVER_URL || 'https://[webappname].azurewebsites.net') + '/parse',
+    appId: process.env.APP_ID || 'edison',
+    masterKey: process.env.MASTER_KEY || 'mredison',
+    databaseURI: process.env.DATABASE_URI || 'mongodb://localhost:27017/myparse',
+    serverURL: (process.env.SERVER_URL || 'http://localhost:3000') + '/parse',
   },
   dashboard: {
     apps: [
       {
-        serverURL: (process.env.SERVER_URL || 'https://[webappname].azurewebsites.net') + '/parse',
-        appId: process.env.APP_ID || 'username',
-        masterKey: process.env.MASTER_KEY || '**********',
+        serverURL: (process.env.SERVER_URL || 'http://localhost:3000') + '/parse',
+        appId: process.env.APP_ID || 'edison',
+        masterKey: process.env.MASTER_KEY || 'mredison',
         appName: 'Parse Server Azure' 
       }
     ],
     users: [ 
       { 
-        user: process.env.APP_ID || 'username',
-        pass: process.env.MASTER_KEY || '********',
+        user: process.env.APP_ID || 'edison',
+        pass: process.env.MASTER_KEY || 'mredison',
       } 
     ] 
   },
