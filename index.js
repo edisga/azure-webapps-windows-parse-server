3639b82f53920a5845e456f8e866d8f1991f4135
Edison Garcia <egachi@live.com>
2019-07-17 23:34:09 -0500

    Adding package-lock.json to gitignore

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ var url = require('url');
 var config = parseServerConfig(__dirname);
 
 // Modify config as necessary before initializing parse server & dashboard
+
 var options = { allowInsecureHTTP: true };
 var app = express();
 app.use('/public', express.static(__dirname + '/public'));
 app.use('/parse', new ParseServer(config.server));
 app.use('/parse-dashboard', ParseDashboard(config.dashboard, options));
+
 //app.use('/parse-dashboard', ParseDashboard(config.dashboard, true));
 
 app.listen(process.env.PORT || url.parse(config.server.serverURL).port, function () {
