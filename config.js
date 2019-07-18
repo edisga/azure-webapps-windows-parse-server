2220006799e2713b4f0f9779949a57ad7ee37937
Edison Garcia <egachi@live.com>
2019-07-17 23:07:21 -0500

    Adding package-lock.json to gitignore

--- config.js
+++ config.js
@@ -2,13 +2,13 @@ module.exports = {
   server: {
     appId: process.env.APP_ID || 'edison',
     masterKey: process.env.MASTER_KEY || 'mredison',
-    databaseURI: process.env.DATABASE_URI || 'mongodb://localhost:27017/myparse',
-    serverURL: (process.env.SERVER_URL || 'http://localhost:3000') + '/parse',
+    databaseURI: process.env.DATABASE_URI || 'mongodb://edisga:xJEvdeLVKXh6ycOUtXZLD8TVdEoofh4D8n1HqYXP7rYtNzVkPq7j2l7iKkVPgwZ3UWeG9FPTm70A1DZKfUYt4Q==@edisga.documents.azure.com:10255/myparse?ssl=true&replicaSet=globaldb',
+    serverURL: (process.env.SERVER_URL || 'https://edisga-parser.azurewebsites.net') + '/parse',
   },
   dashboard: {
     apps: [
       {
-        serverURL: (process.env.SERVER_URL || 'http://localhost:3000') + '/parse',
+        serverURL: (process.env.SERVER_URL || 'https://edisga-parser.azurewebsites.net') + '/parse',
         appId: process.env.APP_ID || 'edison',
         masterKey: process.env.MASTER_KEY || 'mredison',
         appName: 'Parse Server Azure' 
