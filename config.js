d59808e6f242ffa5213317eda56c5db1437cf1fd
Edison Garcia <egachi@live.com>
2019-07-18 00:09:58 -0500

    Updating to Github

--- config.js
+++ config.js
@@ -1,23 +1,23 @@
 module.exports = {
   server: {
-    appId: process.env.APP_ID || 'edison',
-    masterKey: process.env.MASTER_KEY || 'mredison',
-    databaseURI: process.env.DATABASE_URI || 'mongodb://edisga:xJEvdeLVKXh6ycOUtXZLD8TVdEoofh4D8n1HqYXP7rYtNzVkPq7j2l7iKkVPgwZ3UWeG9FPTm70A1DZKfUYt4Q==@edisga.documents.azure.com:10255/myparse?ssl=true&replicaSet=globaldb',
-    serverURL: (process.env.SERVER_URL || 'https://edisga-parser.azurewebsites.net') + '/parse',
+    appId: process.env.APP_ID || 'username',
+    masterKey: process.env.MASTER_KEY || 'password',
+    databaseURI: process.env.DATABASE_URI || 'mongodb://username:password@[servername].documents.azure.com:10255/[dbname]?ssl=true&replicaSet=globaldb',
+    serverURL: (process.env.SERVER_URL || 'https://[webappname].azurewebsites.net') + '/parse',
   },
   dashboard: {
     apps: [
       {
-        serverURL: (process.env.SERVER_URL || 'https://edisga-parser.azurewebsites.net') + '/parse',
-        appId: process.env.APP_ID || 'edison',
-        masterKey: process.env.MASTER_KEY || 'mredison',
+        serverURL: (process.env.SERVER_URL || 'https://[webappname].azurewebsites.net') + '/parse',
+        appId: process.env.APP_ID || 'username',
+        masterKey: process.env.MASTER_KEY || 'password',
         appName: 'Parse Server Azure' 
       }
     ],
     users: [ 
       { 
-        user: process.env.APP_ID || 'edison',
-        pass: process.env.MASTER_KEY || 'mredison',
+        user: process.env.APP_ID || 'username',
+        pass: process.env.MASTER_KEY || 'password',
       } 
     ] 
   },
